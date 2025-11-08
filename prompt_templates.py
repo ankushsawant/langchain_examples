fed5e3ec995b33e5bea48157dc9dde761edf7ee7
Ankush Sawant <ankushsawant@yahoo.com>
2025-11-07 17:28:45 -0800

    added in prompt templates with chat examples

--- prompt_templates.py
+++ prompt_templates.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
@@ -11,15 +10,28 @@ def prompt_templates():
     # Create a ChatGoogleGenAI model
     model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-    # Templates with multiple placeholders
-    multiple_inputs = """You are a helpful AI assistant.
-    Human: Tell me a {adjective} joke about {animal}.
-    Assistant:"""
+    # Part 1
+    # Joke prompt template with multiple placeholders or variables
+    multi_inputs = "Tell us {number_of_jokes} funny jokes about {subject}"
 
-    prompt_multiple = ChatPromptTemplate.from_template(multiple_inputs)
-    prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
+    joke_template = ChatPromptTemplate.from_template(multi_inputs)
+    joke_prompt = joke_template.invoke({"number_of_jokes": "2", "subject": "doctors"})
+    # print(joke_prompt)
 
-    result = model.invoke(prompt)
+    result = model.invoke(joke_prompt)
+    print(result.content)
+
+    # Part 2
+    # Joke prompt template with System and Human Messages (Using Tuples)
+    joke_messages = [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+    joke_template_messages = ChatPromptTemplate.from_messages(joke_messages)
+    joke_prompt_messages = joke_template_messages.invoke({"topic": "lawyers", "joke_count": 3})
+    # print(joke_prompt_messages)
+
+    result = model.invoke(joke_prompt_messages)
     print(result.content)
 
 if __name__ == "__main__":
